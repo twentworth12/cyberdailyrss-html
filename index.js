91441dab5886648f6ee4389395f411e60e8e503a
twentworth12 <twentworth12@gmail.com>
2023-02-08 14:47:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+var express = require('express')
+var app = express();
+
 import fetch from 'node-fetch';
 import RSSParser from 'rss-parser';
 const RSSparser = new RSSParser();
 
 import { parse } from 'node-html-parser';
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+
+
 
 fetch("https://therecord.media/feed")
   .then(res => res.text())
@@ -12,7 +22,6 @@ fetch("https://therecord.media/feed")
         
     let html = "";
     feed.items.forEach(item => {
-    
 
 	const root = parse(item.content);
 	let imgsrc = root.querySelector('img').getAttribute('src');
@@ -21,6 +30,11 @@ fetch("https://therecord.media/feed")
 	  html += `<h2><span style="font-size: 18px; color: #3366ff;"><a href='${item.link}' style="color:#3366ff" >${item.title}<img width="600" src="${imgsrc}"></a></span></h2><p style="margin-bottom: 1em;">${item.contentSnippet}</p>`;
     });
     html += "";
-    console.log(html);
-    
-  });
+    response.send(html)
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
+
+});
