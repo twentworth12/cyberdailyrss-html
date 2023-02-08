8712bfce91ae03f7d381f5c8f8ac27988c7df26e
twentworth12 <twentworth12@gmail.com>
2023-02-08 15:10:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import express from 'express';
-const app = express();
+var express = require('express')
+var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
